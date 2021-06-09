530945a665f2533012479bc7d0e8fc59b548171d
andreihrs <andrei_hristescu@yahoo.com>
2021-06-09 10:56:55 +0300

    To fix gt bug

--- graphql.js
+++ graphql.js
@@ -4,25 +4,39 @@ const {
   useQuery,
   InMemoryCache,
 } = require("@apollo/client/core");
-
+const fs = require("fs");
+const path = require("path");
 require("cross-fetch/polyfill");
+const Web3 = require("web3");
+const NODE_URL = "https://matic-mainnet-full-rpc.bwarelabs.com";
+const provider = new Web3.providers.HttpProvider(NODE_URL);
+const web3 = new Web3(provider);
 
-const APIURL = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2";
+const APIURL = "https://api.thegraph.com/subgraphs/name/sameepsi/quickswap";
 
 const pairsQuery = `
-  query {
-    pairs(first: 100, orderBy: reserveUSD, orderDirection: desc, filter: {
-      createdAtBlockNumber: {
-        gt: 12593815
-      }
+  query($blockNumber: Int!) {
+    pairs(first: 100, orderBy: reserveUSD, orderDirection: desc,
+      where : {
+        createdAtBlockNumber_gt: $blockNumber
     }) {
       id 
-      token0
-      token1
+      reserveUSD
+      token0 {
+        id
+        symbol
+        name
+      }
+      token1 {
+        id
+        symbol
+        name
+      }
       createdAtTimestamp
       createdAtBlockNumber
       liquidityProviderCount
       volumeUSD
+      
     }
   }
 `;
@@ -32,14 +46,22 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-client
-  .query({
-    query: gql(pairsQuery),
-    variables: {
-      skip: 300,
-    },
-  })
-  .then((data) => console.log("Subgraph data", data.data))
-  .catch((err) => {
-    console.log("Error fetching data: ", err);
-  });
+web3.eth.getBlockNumber().then((blockResult) => {
+  console.log("Latest Polygon Block is ", blockResult);
+  client
+    .query({
+      query: gql(pairsQuery),
+      variables: {
+        blockNumber: blockResult - 2000,
+      },
+    })
+    .then((data) =>
+      fs.writeFileSync(
+        path.resolve(__dirname, "data.json"),
+        JSON.stringify(data.data)
+      )
+    )
+    .catch((err) => {
+      console.log("Error fetching data: ", err);
+    });
+});
