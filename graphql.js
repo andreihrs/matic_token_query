055e44f50e7ae9381cc7b027ff340b280ddc4929
andreihrs <andrei_hristescu@yahoo.com>
2021-06-09 11:47:28 +0300

    Get all the pairs listed on dfyn, quick, sushi within last 10000 blocks

--- graphql.js
+++ graphql.js
@@ -12,14 +12,20 @@ const NODE_URL = "https://matic-mainnet-full-rpc.bwarelabs.com";
 const provider = new Web3.providers.HttpProvider(NODE_URL);
 const web3 = new Web3(provider);
 
-const APIURL = "https://api.thegraph.com/subgraphs/name/sameepsi/quickswap";
+const APIURL_QUICK =
+  "https://api.thegraph.com/subgraphs/name/sameepsi/quickswap";
+const APIURL_SUSHI =
+  "https://api.thegraph.com/subgraphs/name/sushiswap/matic-exchange";
+const APIURL_DFYN = "https://api.thegraph.com/subgraphs/name/ss-sonic/dfyn-v5";
 
-const pairsQuery = `
+const pairsQuery_quick = `
   query($blockNumber: Int!) {
-    pairs(first: 100, orderBy: reserveUSD, orderDirection: desc,
+    pairs(first: 100, orderBy: createdAtTimestamp, orderDirection: desc,
       where : {
         createdAtBlockNumber_gt: $blockNumber
-    }) {
+      }
+    ) 
+    {
       id 
       reserveUSD
       token0 {
@@ -41,27 +47,63 @@ const pairsQuery = `
   }
 `;
 
-const client = new ApolloClient({
-  uri: APIURL,
-  cache: new InMemoryCache(),
-});
+const pairsQuery_sushi = `
+  query($blockNumber: Int!) {
+    pairs(first: 100, orderBy: timestamp, orderDirection: desc,
+      where : {
+        block_gt: $blockNumber
+      }
+    ) 
+    {
+      id 
+      reserveUSD
+      token0 {
+        id
+        symbol
+        name
+      }
+      token1 {
+        id
+        symbol
+        name
+      }
+      timestamp
+      block
+      liquidityProviderCount
+      volumeUSD
+      
+    }
+  }
+`;
 
-web3.eth.getBlockNumber().then((blockResult) => {
-  console.log("Latest Polygon Block is ", blockResult);
-  client
-    .query({
-      query: gql(pairsQuery),
-      variables: {
-        blockNumber: blockResult - 2000,
-      },
-    })
-    .then((data) =>
-      fs.writeFileSync(
-        path.resolve(__dirname, "data.json"),
-        JSON.stringify(data.data)
-      )
-    )
-    .catch((err) => {
-      console.log("Error fetching data: ", err);
-    });
-});
+const queryDEX = (apiUrl, exchange, pairQuery) => {
+  const client = new ApolloClient({
+    uri: apiUrl,
+    cache: new InMemoryCache(),
+  });
+
+  web3.eth.getBlockNumber().then((blockResult) => {
+    console.log("Latest Polygon Block is ", blockResult);
+    client
+      .query({
+        query: gql(pairQuery),
+        variables: {
+          blockNumber: blockResult - 10000,
+        },
+      })
+      .then((data) => {
+        console.log(data.data);
+        fs.writeFileSync(
+          path.resolve(__dirname, "data-" + `${exchange}` + ".json"),
+          JSON.stringify(data.data)
+        );
+      })
+      .catch((err) => {
+        console.log("Error fetching data: ", err);
+      });
+  });
+};
+
+queryDEX(APIURL_SUSHI, "sushi", pairsQuery_sushi);
+queryDEX(APIURL_QUICK, "quick", pairsQuery_quick);
+queryDEX(APIURL_DFYN, "dfyn", pairsQuery_quick);
